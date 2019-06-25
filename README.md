65309cbe00efd00b2bf086fec63cbcfe53037b8a
Yair Chuchem <yairchu@gmail.com>
2019-06-25 15:15:11 +0300

    initial stack-compat tool

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # stack-compat
+
+Build old Stack 1 projects with Stack 2
+
+Used for `git bisect`ing over history where newer versions of stack are installed.
+
+## Related tips
+
+### macOS
+
+If Stack 2.1.1 fails with `tar: Option --force-local is not supported`,
+then is a bug with newer version of Stack
+which can be worked around via `brew install gnu-tar` and
+creating a symlink to it named `tar` in your path shadowing macOS's bundled `tar`.
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Control.Exception (finally)
+import qualified Control.Lens as Lens
+import           Control.Lens.Operators
+import qualified Data.Aeson.Lens as AesonLens
+import qualified Data.ByteString as BS
+import qualified Data.Vector as Vector
+import qualified Data.Yaml as Yaml
+import           System.Directory (getHomeDirectory, removeFile)
+import           System.Environment (getArgs)
+import           System.Process (callProcess)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main =
+    do
+        srcRaw <- BS.readFile "stack.yaml"
+        src <- Yaml.decodeThrow srcRaw :: IO Yaml.Value
+        src
+            & AesonLens.key "packages" . AesonLens._Array %~
+                Vector.filter (Lens.nullOf (AesonLens.key "location"))
+            & AesonLens.key "extra-deps" . AesonLens._Array <>~
+                Vector.fromList
+                (src ^..
+                    AesonLens.key "packages" .
+                    AesonLens.values . AesonLens.key "location")
+            & Yaml.encodeFile "stack.yaml"
+        home <- getHomeDirectory
+        getArgs >>= callProcess (home <> "/.local/bin/stack")
+            & (`finally` BS.writeFile "stack.yaml" srcRaw)
+        -- Remove "stack.yaml.lock" artifact left by Stack 2,
+        -- for usage in scripts which expect behavior to match Stack 1's.
+        removeFile "stack.yaml.lock"
